f6f21f4ab9fcb8b93077caf3d4a4bdcbf44a9172
teguegni <kenfackteguegni@gmail.com>
2025-01-29 01:05:10 +0100

    Update bank-additional.py

--- bank-additional.py
+++ bank-additional.py
@@ -1,7 +1,6 @@
 import pandas as pd  
 import streamlit as st  
-import altair as alt  
-from sklearn.model_selection import train_test_split  
+import altair as alt    
 from sklearn.ensemble import RandomForestClassifier  
 from sklearn.preprocessing import LabelEncoder  
 
@@ -50,6 +49,7 @@ age_job_chart = alt.Chart(df).mark_circle(size=60).encode(
 st.write(age_job_chart)  
 
 # Prediction Page  
+from sklearn.model_selection import train_test_split
 if page_selection == "Prediction":  
     st.subheader("Make a Prediction")  
     
