c8f1192a9d663bbc0e0bf90dc6e691e47d61b503
teguegni <kenfackteguegni@gmail.com>
2025-01-29 02:54:40 +0100

    Update bank-additional.py

--- bank-additional.py
+++ bank-additional.py
@@ -2,7 +2,7 @@
 import streamlit as st  
 import pandas as pd  
 import altair as alt  
-import seaborn as sns  
+#import seaborn as sns  
 import matplotlib.pyplot as plt  
 
 # Configuration de la page  
